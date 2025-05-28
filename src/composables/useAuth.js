04591205aede229cc411a0c904096bfe96f17ec3
Александр Белькевич <alexbelkevich@MacBook-Pro-Aleksandr.local>
2025-05-28 18:41:24 +0400

    2.12

--- src/composables/useAuth.js
+++ src/composables/useAuth.js
@@ -3,12 +3,13 @@ import { useRequest } from '@/composables/useRequest.js'
 
 export function useAuth() {
   const { loading, errorMessage, handleRequest } = useRequest()
-  const signUp = async ({ email, password }) => {
+  const signUp = async ({ email, password, firstname }) => {
     return await handleRequest(async () => {
       const { data, error } = await supabase.auth.signUp({
         email,
         password,
       })
+      await supabase.from('users').insert([{ id: data.user.id, firstname, email }])
       if (error) throw error
       return data
     })
