5c351cccef6cc755c81f46876c61d8dbc833ed3c
Александр Белькевич <alexbelkevich@MacBook-Pro-Aleksandr.local>
2025-07-03 16:10:53 +0400

    4.3-4.4

--- src/components/Modals/CategoriesModal.vue
+++ src/components/Modals/CategoriesModal.vue
@@ -1,26 +1,39 @@
 <script setup>
 import { ref } from 'vue'
+import { supabase } from '@/supabase.js'
+import { useToastNofitications } from '@/composables/useToastNofitications.js'
 import Dialog from 'primevue/dialog'
 import InputText from 'primevue/inputtext'
 import Button from 'primevue/button'
+import Toast from 'primevue/toast'
 import { Form } from '@primevue/forms'
 
 const modelValue = defineModel()
+const { showToast } = useToastNofitications()
 
+const isLoading = ref(false)
 const categoryName = ref('')
-const listCategories = ref([
-  {
-    id: 1,
-    name: 'Тестовая категория',
-  },
-  {
-    id: 2,
-    name: 'Тестовая категория 2',
-  },
-])
+const listCategories = ref([])
 
-const saveCategory = () => {
-  console.log('saveCategory')
+const saveCategory = async () => {
+  isLoading.value = true
+
+  try {
+    const { data, error } = await supabase
+      .from('categories')
+      .insert({ name: categoryName.value })
+      .select()
+
+    if (error) throw error
+    listCategories.value.push(data[0])
+    categoryName.value = ''
+
+    showToast('success', 'Успех', 'Категория добавлена')
+  } catch {
+    showToast('error', 'Ошибка', 'Не удалось добавить категорию')
+  } finally {
+    isLoading.value = false
+  }
 }
 
 const deleteCategory = (id) => {
@@ -29,6 +42,7 @@ const deleteCategory = (id) => {
 </script>
 
 <template>
+  <Toast />
   <Dialog header="Категории" v-model:visible="modelValue" :style="{ width: '25rem' }">
     <Form @submit="saveCategory">
       <div class="gap-2 mb-2">
@@ -40,7 +54,7 @@ const deleteCategory = (id) => {
         />
       </div>
       <div class="flex justify-end gap-2 mt-4">
-        <Button type="button" label="Добавить" @click="saveCategory" />
+        <Button type="button" label="Добавить" :loading="isLoading" @click="saveCategory" />
       </div>
       <div
         class="grid mt-3 grid-cols-[1fr_32px] mb-1 gap-5"
