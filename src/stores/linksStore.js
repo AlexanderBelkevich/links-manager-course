001ee3851e7c4dcd49aaced247d91c6b695a1464
Александр Белькевич <alexbelkevich@MacBook-Pro-Aleksandr.local>
2025-08-05 19:38:13 +0400

    6.3

--- src/stores/linksStore.js
+++ src/stores/linksStore.js
@@ -22,47 +22,10 @@ export const useLinksStore = defineStore('links', () => {
     isLoading.value = false
   }
 
-  const changeIsFavorite = async (id) => {
-    const index = links.value.findIndex((link) => link.id === id)
-    if (index !== -1) {
-      const newFavorite = !links.value[index].is_favorite
-
-      const { error } = await supabase
-        .from('links')
-        .update({ is_favorite: newFavorite })
-        .eq('id', id)
-      if (error) throw error
-      links.value[index].is_favorite = newFavorite
-    }
-  }
-
-  const addClickCount = async (id) => {
-    const index = links.value.findIndex((link) => link.id === id)
-    if (index !== -1) {
-      const newClickCount = links.value[index].click_count + 1
-
-      const { error } = await supabase
-        .from('links')
-        .update({ click_count: newClickCount })
-        .eq('id', id)
-      if (error) throw error
-      links.value[index].click_count = newClickCount
-    }
-  }
-
-  const removeLink = async (id) => {
-    const { error } = await supabase.from('links').delete().eq('id', id)
-    if (error) throw error
-    links.value = links.value.filter((link) => link.id !== id)
-  }
-
   return {
     isLoading,
     links,
     fetchLinks,
-    changeIsFavorite,
-    addClickCount,
-    removeLink,
   }
 })
 
